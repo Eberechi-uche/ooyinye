97193a53954ee4eb720c6a82919c314e7c130067
Richard Eberechi Uche <eb3rechi@gmail.com>
2023-05-28 13:28:11 +0100

    fix deployment error

--- src/Components/TextEditor/TextEditor.js
+++ src/Components/TextEditor/TextEditor.js
@@ -1,11 +1,1 @@
-import React, { useState } from "react";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
 
-function TextEditor() {
-  const [value, setValue] = useState(" this should be something i hope");
-  console.log(value);
-
-  return <ReactQuill theme="snow" value={value} onChange={setValue} />;
-}
-export default TextEditor;
--- src/pages/profile/Dashboard/index.tsx
+++ src/pages/profile/Dashboard/index.tsx
@@ -1,14 +1,9 @@
 import { Flex, Text } from "@chakra-ui/react";
-import TextEditor from "@/Components/TextEditor/TextEditor";
-import { useRef } from "react";
 const Dashboard: React.FC = () => {
-  const editorRef = useRef(null);
   return (
     <>
       <Text> i am a dashboard </Text>
-      <Flex px={"10"} flexDir={"column"}>
-        <TextEditor />
-      </Flex>
+      <Flex px={"10"} flexDir={"column"}></Flex>
     </>
   );
 };
